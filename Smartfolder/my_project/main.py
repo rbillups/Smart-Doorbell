aaa2eafdd144d1c6f948833e52830b4632727b96
rbillups <96398943+rbillups@users.noreply.github.com>
2024-08-04 18:12:31 -0400

    update py script

--- Smartfolder/my_project/main.py
+++ Smartfolder/my_project/main.py
@@ -2,23 +2,27 @@ import RPi.GPIO as GPIO
 import time
 import os
 
-#adjust for where your switch is connected
-#buttonPin = 4  #for smart lock
+# Set up GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)  # Disable warnings
+
+# Define button pins
+buttonPin = 17
 buttonPina = 25
 
-buttonPin = 17
-#buttonPin = 18
-#buttonPin =26
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(buttonPin,GPIO.IN)
-GPIO.setup(buttonPina,GPIO.IN)
-#GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# Set up GPIO pins
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Use pull-down resistor
+GPIO.setup(buttonPina, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Use pull-down resistor
 
-while True:
-    #assuming the script to call is long enough we can ignore bouncing
-   # if (GPIO.input(buttonPin)):
-    if (GPIO.input(buttonPin) or GPIO.input(buttonPina) ):
-        print("button pressed")
-        #this is the script that will be called (as root)
-        os.system("fswebcam -r 960x720 -d /dev/video0 /home/pi/my_project/nks.jpg")
-        os.system("python /home/pi/my_project/emailscript.py")
+try:
+    while True:
+        # Check if either button is pressed
+        if GPIO.input(buttonPin) or GPIO.input(buttonPina):
+            print("button pressed")
+            # Capture image and run email script
+            #os.system("fswebcam -r 960x720 -d /dev/video0 /home/pi/my_project/nks.jpg")
+            os.system("python /home/pi/my_project/emailscript.py")
+        time.sleep(0.1)  # Add a small delay to debounce
+except KeyboardInterrupt:
+    # Clean up GPIO on exit
+    GPIO.cleanup()
