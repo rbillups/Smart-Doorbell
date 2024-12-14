d7495be792fae09b01f3321ad2e7bc6c0826a0bd
Reginald Billups <keybillups@Mac.attlocal.net>
2024-12-13 20:11:04 -0500

    hill test2

--- Smartfolder/my_project/hill_test.py
+++ Smartfolder/my_project/hill_test.py
@@ -1,16 +1,28 @@
 import RPi.GPIO as GPIO
 import time
+import os
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN)  # GPIO 17 as input
+GPIO.setwarnings(False)  # Disable warnings
+
+# Define button pins
+buttonPin = 17
+buttonPina = 25
+
+# Set up GPIO pins
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Use pull-down resistor
+GPIO.setup(buttonPina, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Use pull-down resistor
 
 try:
     while True:
-        if GPIO.input(17):  # Check if the input is HIGH (3.3V)
-            print("Button Pressed")
-        else:
-            print("Button Not Pressed")
-        time.sleep(0.1)  # Delay for a short time before checking again
+        # Check if either button is pressed
+        if GPIO.input(buttonPin) or GPIO.input(buttonPina):
+            print("button pressed")
+            # Capture image and run email script
+            #os.system("fswebcam -r 960x720 -d /dev/video0 /home/pi/my_project/nks.jpg")
+            os.system("python /home/pi/my_project/emailscript.py")
+        time.sleep(0.1)  # Add a small delay to debounce
 except KeyboardInterrupt:
+    # Clean up GPIO on exit
     GPIO.cleanup()
